c830faebfe7f44b2572d0db5e6467b9d669bbf94
MarceloJoia <sitejoia@hotmail.com>
2022-04-05 16:14:52 -0300

    beecrowd | 1042 - Sort Simples

---
+++ beecrowd1042_SortSimples/src/Main.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n1, n2, n3;
+
+		n1 = sc.nextInt();
+		n2 = sc.nextInt();
+		n3 = sc.nextInt();
+
+		if (n1 < n2 && n1 < n3) {
+
+			if(n2 < n3) {
+				System.out.println(n1);
+				System.out.println(n2);
+				System.out.println(n3);
+				System.out.println();
+				System.out.println(n1);
+				System.out.println(n2);
+				System.out.println(n3);
+			} else {
+				System.out.println(n1);
+				System.out.println(n3);
+				System.out.println(n2);
+				System.out.println();
+				System.out.println(n1);
+				System.out.println(n2);
+				System.out.println(n3);
+			}
+			
+		} 
+		else if (n2 < n1 && n2 < n3) {
+			if(n1 < n3) {
+				System.out.println(n2);
+				System.out.println(n1);
+				System.out.println(n3);
+				System.out.println();
+				System.out.println(n1);
+				System.out.println(n2);
+				System.out.println(n3);
+			} else {
+				System.out.println(n2);
+				System.out.println(n3);
+				System.out.println(n1);
+				System.out.println();
+				System.out.println(n1);
+				System.out.println(n2);
+				System.out.println(n3);
+			}
+		} 
+		else if (n3 < n1 && n3 < n2) {
+			if(n1 < n2) {
+				System.out.println(n3);
+				System.out.println(n1);
+				System.out.println(n2);
+				System.out.println();
+				System.out.println(n1);
+				System.out.println(n2);
+				System.out.println(n3);
+			} else {
+				System.out.println(n3);
+				System.out.println(n2);
+				System.out.println(n1);
+				System.out.println();
+				System.out.println(n1);
+				System.out.println(n2);
+				System.out.println(n3);
+			}
+		}
+
+		sc.close();
+
+	}
+
+}
