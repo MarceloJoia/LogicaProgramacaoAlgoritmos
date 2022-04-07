791657520f40d77c2eacbcc4289eaacc2ac8e259
MarceloJoia <sitejoia@hotmail.com>
2022-04-07 14:12:36 -0300

    for - beecrowd | 1071 - Soma de Impares Consecutivos I

---
+++ for_beecrowd1071_SomaDeImparesConsecutivos-I/src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		/*
+		 -5 -4 -3 -2 -1 0 1 2 3 4 5 6
+		 -3 -1 1 3 5
+		*/
+		
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		
+		int min, max;
+		if(x < y) {
+			min = x;
+			max = y;
+		} else {
+			min = y;
+			max = x;
+		}
+		
+		int soma = 0;
+		for(int i = min+1; i < max; i++) {
+			if(i % 2 != 0) {
+				soma += i;
+			}
+		}
+		System.out.println(soma);
+		
+		sc.close();
+	}
+
+}
