4f5dd15fa9137bcfecd29ce921caeb9cda0c1f11
MarceloJoia <sitejoia@hotmail.com>
2022-04-08 08:50:34 -0300

    for / Beecrowd | 1116 - Dividindo X por Y

---
+++ for_beecrowd1116_DividindoXporY/src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		for(int i = 0; i < N; i++) {
+			
+			double X = sc.nextDouble();
+			double Y = sc.nextDouble();
+			
+			if(Y != 0) {
+				double result = X / Y;
+				System.out.printf("%.1f%n",result);
+			} else {
+				System.out.println("divisao impossivel");
+			}
+		}
+		
+		sc.close();
+
+	}
+
+}
