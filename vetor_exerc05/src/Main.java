58947a07ac3cfe895e57710d40a6d145485e87f2
MarceloJoia <sitejoia@hotmail.com>
2022-04-16 17:02:36 -0300

    Vetor - média aritmética somente dos números pares lidos.

---
+++ vetor_exerc05/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int[] vector = new int[N];
+		double[] pares = new double[N];
+		
+		for(int i=0; i<N; i++) {
+			vector[i] = (int)sc.nextDouble();
+		}
+		
+		int count = 0;
+		for(int i=0; i<N; i++) {
+			if(vector[i] % 2 == 0) {
+				pares[i] = (double)vector[i];
+				count++;
+			}
+		}
+		
+		double media = 0;
+		for(int i=0; i<N; i++) {
+			media += pares[i];
+		}
+		System.out.printf("%.1f%n", media/count);
+		
+		sc.close();
+	}
+}
