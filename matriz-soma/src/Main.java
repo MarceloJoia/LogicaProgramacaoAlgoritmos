eb9e36f19b4774d07a35969d25d7bc2c65e002b0
MarceloJoia <sitejoia@hotmail.com>
2022-04-20 08:58:44 -0300

    Matriz Soma

---
+++ matriz-soma/src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int M = sc.nextInt();
+		int N = sc.nextInt();
+		
+		double[][] mat = new double[M][N];
+		
+		for(int i=0; i<M; i++) {
+			for(int j=0; j<N; j++) {
+				mat[i][j] = sc.nextDouble();
+			}
+		}
+		
+		double[] vet = new double[M];
+		for(int i=0; i<M; i++) {
+			for(int j=0; j<N; j++) {
+				vet[i] += mat[i][j];
+			}
+		}
+		
+		for(int i=0; i<M; i++) {
+			System.out.printf("%.1f%n", vet[i]);
+		}
+		
+		sc.close();
+
+	}
+}
